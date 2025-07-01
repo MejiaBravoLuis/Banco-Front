78a96173d145354040e0cf36bf6502ef77c9bd83
hmonroy-2020131 <hmonroy-2020131@kinal.edu.gt>
2025-07-01 07:54:41 -0600

    Probelmas arreglados en las rutas

--- src/components/PrivateRoutes.jsx
+++ src/components/PrivateRoutes.jsx
@@ -1,7 +1,13 @@
 import { Navigate } from 'react-router-dom';
 import { useUserDetails } from '../shared/hooks';
 
-export const PrivateRoute = ({ children }) => {
-  const { isLogged } = useUserDetails();
-  return isLogged ? children : <Navigate to="/auth" replace />;
-};
+export const PrivateRoute = ({ children, allowedRoles = [] }) => {
+    const { isLogged, role } = useUserDetails();
+  
+    if (!isLogged) return <Navigate to="/auth" replace />;
+    if (allowedRoles.length > 0 && !allowedRoles.includes(role.toUpperCase())) {
+      return <Navigate to="/dashboard" replace />;
+    }
+  
+    return children;
+  };
